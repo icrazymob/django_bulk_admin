3fab703a971c60f437aa0227001b0a099eb330f3
icrazymob <44069831+icrazymob@users.noreply.github.com>
2021-05-01 17:00:06 +0300

    fix

--- bulk_admin/admin.py
+++ bulk_admin/admin.py
@@ -5,18 +5,19 @@ from django import forms
 from django.contrib import admin, messages
 from django.contrib.admin.exceptions import DisallowedModelAdminToField
 from django.contrib.admin.options import IS_POPUP_VAR, InlineModelAdmin, TO_FIELD_VAR, csrf_protect_m
-from django.contrib.admin.templatetags.admin_static import static
+from django.templatetags.static import static
+#from django.contrib.admin.templatetags.admin_static import static
 from django.contrib.admin.templatetags.admin_urls import add_preserved_filters
 from django.contrib.admin.utils import NestedObjects, flatten_fieldsets
 from django.core.exceptions import PermissionDenied, ValidationError
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.db import router, transaction
 from django.forms.formsets import DELETION_FIELD_NAME, INITIAL_FORM_COUNT, TOTAL_FORM_COUNT, ManagementForm
 from django.forms.models import modelform_defines_fields, modelformset_factory, BaseModelFormSet
 from django.forms.utils import ErrorList
 from django.http import HttpResponseRedirect
 from django.template.response import SimpleTemplateResponse
-from django.utils import six
+import six
 from django.utils.encoding import force_text
 from django.utils.text import get_text_list
 from django.utils.translation import ugettext as _, ugettext_lazy
@@ -312,7 +313,7 @@ class BulkModelAdmin(admin.ModelAdmin):
     @property
     def media(self):
         media = super(BulkModelAdmin, self).media
-        media.add_js([static('bulk_admin/js/bulk.js')])
+        media._js.extend([static('bulk_admin/js/bulk.js')])
 
         return media
 
