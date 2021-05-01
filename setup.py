703a35716700bf4cf78577b18b6e0c85e9571576
icrazymob <44069831+icrazymob@users.noreply.github.com>
2021-05-01 18:11:56 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-bulk-admin',
     version='0.1.2',
-    packages=find_packages(exclude=()),
+    packages=['bulk_admin'],
     include_package_data=True,
     license='BSD',
     description='Django bulk admin enables you to bulk add, bulk edit, bulk upload and bulk select in django admin.',
